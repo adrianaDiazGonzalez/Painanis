593268f77c112783d7d97452282d7c17d9454709
Adriana Diaz Gonzalez <adriana.diaz.gonzalez4@gmail.com>
2017-08-04 14:18:11 -0500

    Ventana valida

--- app/plantillas/sg/valida.component.ts
+++ app/plantillas/sg/valida.component.ts
@@ -1,12 +1,34 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
+//Declaración de los componentes de la vista
 @Component({
   selector: "my-app",
-  template: `
-    <ActionBar title="Validación" class="action-bar"></ActionBar>
-    
+  template:
+  ` 
+   <ScrollView>
+   <StackLayout >
+    <ActionBar title="Dirección" class="action-bar"></ActionBar>
+    <Image src="~/images/book.png" class="ubicacion" horizontalAlignment="center" verticalAlignment="center" ></Image>
+    <Label text="¡Valida tu dirección!" horizontalAlignment="center"> </Label>
+    <TextField hint="CP" [(ngModel)]="cp"></TextField >
+    <Button text = "Validar" class="submit-button" (tap)="validar()"></Button>
+   
+    </StackLayout>
+  </ScrollView>
+ 
   `
 })
-export class ValidaComponent {
 
-}
+
+export class validaComponent {
+    validar()
+    {
+        var cp
+        console.log(cp);
+
+    }
+
+    
+  
+  }
