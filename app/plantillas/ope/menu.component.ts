86bc09e94249d4b002284df84252b53f6ea58076
Adriana Diaz Gonzalez <adriana.diaz.gonzalez4@gmail.com>
2017-08-21 13:56:54 -0500

    Cambios en menu Adriana

--- app/plantillas/ope/menu.component.ts
+++ app/plantillas/ope/menu.component.ts
@@ -3,8 +3,7 @@ import { Router } from "@angular/router";
 import { HttpModule, Http } from '@angular/http';
 import { Page } from "ui/page";
 import { MenuService } from "../../servicios/ope/menu";
-
-
+import { Domicilio } from "../../modelos/ope/menu";
 
 @Component({
     selector: "my-app",
@@ -14,10 +13,75 @@ import { MenuService } from "../../servicios/ope/menu";
 })
 
 export class MenuComponent {
-    constructor(private router: Router) {
-
+    domicilio: Domicilio;
+    constructor(private page: Page, private router: Router, private menuService: MenuService) {
+        page.actionBarHidden = true; //sirve para ocultar la barra de titulo de la ventana
+        this.domicilio = new Domicilio();
+        this.domicilio.iDomicilio = "0";
+        this.domicilio.iPersona = "0";
+        this.domicilio.iTipoPersona = "1";
     }
 
+
+    //Acciones o procesos  
+    guardar() {
+        var bandera = false;
+        //Código que condiciona al usuario a ingresar los datos que son obligatorios para el registro de la direción
+        //valida que ingrese CP
+        console.log(this.domicilio.cCp);
+        if (this.domicilio.cCp == null || this.domicilio.cCp == "") {
+            bandera = true;
+            alert("Ingrese un Codigo Postal por favor");
+            return;
+        }
+        else {
+            this.menuService.validacp(this.domicilio.cCp);
+        }
+        //Valida que se ingrese Colonia
+        if (this.domicilio.cColonia == null || this.domicilio.cColonia == "") {
+            alert("Ingrese una Colonia por favor");
+            return;
+        }
+        //Valida que se ingrese Calle
+        if (this.domicilio.cCalle == null || this.domicilio.cCalle == "") {
+            alert("Ingrese una Calle por favor");
+            return;
+        }
+        //Valida que se ingrese un Numero Exterior
+        if (this.domicilio.cNumExterior == null || this.domicilio.cNumExterior == "") {
+            alert("Ingrese un Numero Exterior por favor");
+            return;
+        }
+        //Valida que se ingrese un Municipio
+        if (this.domicilio.cMunicipio == null || this.domicilio.cMunicipio == "") {
+            alert("Ingrese un Municipio por favor");
+            return;
+        }
+        //Valida que se ingrese una Ciudad
+        if (this.domicilio.cCiudad == null || this.domicilio.cCiudad == "") {
+            alert("Ingrese una Ciudad por favor");
+            return;
+        }
+        //Valida que se ingrese un Estado
+        if (this.domicilio.cEstado == null || this.domicilio.cEstado == "") {
+            alert("Ingrese un Estado por favor");
+            return;
+        }
+        //Valida que se ingrese un Pais
+        if (this.domicilio.cPais == null || this.domicilio.cPais == "") {
+            alert("Ingrese un Pais por favor");
+            return;
+        }
+        //Valida que se ingrese un Alias
+        if (this.domicilio.iAlias == null || this.domicilio.iAlias == "") {
+             bandera = true;
+            alert("Ingrese un Alias por favor");
+            return;
+        }
+        //si todas las validaciones son correctas para a la insercion de datos 
+        this.menuService.postQuote(this.domicilio);
+
+    }
     back() {
         this.router.navigate(["sg/usuario"]);
     }
--- app/servicios/ope/menu.ts
+++ app/servicios/ope/menu.ts
@@ -4,12 +4,65 @@ import { Observable } from "rxjs/Rx";
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
 import 'rxjs/add/operator/toPromise';
-
+import { Router } from "@angular/router";
+import { Domicilio } from "../../modelos/ope/menu";
 
 @Injectable()
 export class MenuService {
 
     //constructor del http para los servicios REST
-    constructor(private http: Http) { }
+    constructor(private http: Http, private router: Router) { }
+    postQuote(domicilio: Domicilio) {
+        var tt_ctDomicilio = [domicilio];
+        console.log(JSON.stringify({
+            "request":
+            {
+                "dsDomicilio":
+                { tt_ctDomicilio }
+            }
+        }));
 
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        this.http.post("http://192.168.2.153:8810/Painanis/rest/painanis/as_ctPersona_gen",
+            JSON.stringify({ "request": { "dsPersona": { tt_ctDomicilio } } }),
+            { headers: headers })
+            .map(response => response.json())
+            .subscribe(result => {
+                console.log(JSON.stringify(result));
+                console.log(result.response.oplResultado);
+                console.log(result.response.opcMensaje);
+
+                if (result.response.opcMensaje != "") {
+                    alert("Datos incorrectos");
+                }
+                else {
+                    alert("Desea ingresar otra dirección ");
+                    this.router.navigate(["ope/menu"]);
+                }
+            }, error => {
+                console.log("ERROR: ", error);
+            });
+    }
+
+    validacp(ipcCP: string) {
+        let retorno;
+        let headers = new Headers();
+        headers.append("ipcCP", ipcCP);
+        //llama al servicio REST
+        this.http.get("http://192.168.2.153:8810/Painanis/rest/painanis/as_ctParametrosSis_gen",
+            { headers: headers })
+            .map(response => response.json())
+            .subscribe(result => {
+                //Condicion del resultado de la busqueda de los codigos postales
+                if (result.response.opcMensaje != "") {
+                    alert("Codigo Postal no valido");
+                    return;
+                }
+            }, error => {
+                console.log("ERROR: ", error);
+                retorno = error;
+            });
+    }
 }
+
